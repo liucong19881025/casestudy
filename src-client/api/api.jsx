0534ca4f49f5f749b504952b0175fe99afb004cd
D066520 <cong.liu02@sap.com>
2022-04-04 20:48:32 +0200

    proxy in env

--- src-client/api/api.jsx
+++ src-client/api/api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const url = `${process.env.DB_HOST}`;
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:3000/'
+  baseURL: url
 });
 
 instance.interceptors.request.use((config) => {
